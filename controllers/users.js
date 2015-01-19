ee6bfecb8f6ffd64beb44f2490e92fdf9a76c628
Chris Davies <chris.davies@theironyard.com>
2015-01-19 17:16:50 -0500

    Adding validation to the users controller

--- controllers/users.js
+++ controllers/users.js
@@ -39,7 +39,7 @@ function upsertUser (req, res) {
   };
 
   if (!auth.isValidRole(user.role)) {
-    res.code(400).json({ message: 'Role "' + user.role + '" is not valid' });
+    res.status(400).json({ message: 'Role "' + user.role + '" is not valid' });
   } else {
     promiseResponse(userStore.save(user), res);
   }
