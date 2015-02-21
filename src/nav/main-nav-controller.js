9dd0a61695bf5a4284b9fd935478e55f1c9e1d48
Chris Davies <chris.davies@theironyard.com>
2015-02-21 05:06:13 -0500

    Adding the navigation controller

---
+++ src/nav/main-nav-controller.js
@@ -0,0 +1,13 @@
+app.controller('MainNavCtrl',
+  ['$location', 'StringUtil', function($location, StringUtil) {
+    var self = this;
+
+    self.isActive = function (path) {
+      // The default route is a special case.
+      if (path === '/') {
+        return $location.path() === '/';
+      }
+
+      return StringUtil.startsWith($location.path(), path);
+    };
+  }]);
