5a8bd6759aa9f93672508c3df99f79c50959191e
zachbradshaw <bradshaw.zachary@gmail.com>
2015-03-04 00:13:06 -0500

    updating master for heroku

--- dist/js/app.js
+++ dist/js/app.js
@@ -246,51 +246,6 @@ app.factory('StringUtil', function() {
   };
 });
 
-app.factory('usersService', ['$http', '$q', '$log', function($http, $q, $log) {
-  // My $http promise then and catch always
-  // does the same thing, so I'll put the
-  // processing of it here. What you probably
-  // want to do instead is create a convenience object
-  // that makes $http calls for you in a standard
-  // way, handling post, put, delete, etc
-  function get(url) {
-    return processAjaxPromise($http.get(url));
-  }
-
-  function processAjaxPromise(p) {
-    return p.then(function (result) {
-      return result.data;
-    })
-    .catch(function (error) {
-      $log.log(error);
-    });
-  }
-
-  return {
-    list: function () {
-      return get('/api/users');
-    },
-
-    currentUser: function () {
-      $http.get('api/users/me').then(function (result) {
-        console.log(result.data.userId);
-      })
-    },
-
-    getByUserId: function (userId) {
-      if (!userId) {
-        throw new Error('getByUserId requires a user id');
-      }
-
-      return get('/api/users/' + userId);
-    },
-
-    addUser: function (user) {
-      return processAjaxPromise($http.post('/api/users', user));
-    }
-  };
-}]);
-
 app.factory('commentService', ['$http', '$log', function($http, $log) {
   function post(url, data) {
     return processAjaxPromise($http.post(url, data));
@@ -397,6 +352,51 @@ app.factory('voteService', ['$http', function($http) {
 
     downvote: function (id) {
       return post('/api/res/' + id + '/votes', { vote: -1 });
+    }
+  };
+}]);
+
+app.factory('usersService', ['$http', '$q', '$log', function($http, $q, $log) {
+  // My $http promise then and catch always
+  // does the same thing, so I'll put the
+  // processing of it here. What you probably
+  // want to do instead is create a convenience object
+  // that makes $http calls for you in a standard
+  // way, handling post, put, delete, etc
+  function get(url) {
+    return processAjaxPromise($http.get(url));
+  }
+
+  function processAjaxPromise(p) {
+    return p.then(function (result) {
+      return result.data;
+    })
+    .catch(function (error) {
+      $log.log(error);
+    });
+  }
+
+  return {
+    list: function () {
+      return get('/api/users');
+    },
+
+    currentUser: function () {
+      $http.get('api/users/me').then(function (result) {
+        console.log(result.data.userId);
+      })
+    },
+
+    getByUserId: function (userId) {
+      if (!userId) {
+        throw new Error('getByUserId requires a user id');
+      }
+
+      return get('/api/users/' + userId);
+    },
+
+    addUser: function (user) {
+      return processAjaxPromise($http.post('/api/users', user));
     }
   };
 }]);
